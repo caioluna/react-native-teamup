80c4d6ce8757816b6aa39edb1b8909bc5117f783
Caio Luna <ccluna@gmail.com>
2023-03-09 12:29:32 -0300

    Created app components. Set up app UI

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native'
+
+import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native'
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={{
+        flex: 1,
+        backgroundColor: theme.COLORS.GRAY_600
+      }}>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor='transparent'
+          translucent
+        />
+        {
+          fontsLoaded
+            ? <Groups />
+            : <Loading />
+        }
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
